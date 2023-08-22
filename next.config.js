a20fa3ab93f7f6a1f07f1eebca08ea6ec9f8bbfb
Delowar-maker <56905688+Delowar-maker@users.noreply.github.com>
2023-08-22 06:41:29 +0600

    re

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-//   distDir: "build",
-//   poweredByHeader: false,
-//   async headers() {
-//     return [
-//       {
-//         source: "/:path*",
-//         headers: [
-//           {
-//             key: "X-Frame-Options",
-//             value: "DENY",
-//           },
-//         ],
-//       },
-//     ];
-//   },
+  distDir: "build",
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
